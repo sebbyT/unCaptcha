be5a83780aeb27c06c1fe1b2cb6fbe3720b8fb95
sebbyT <83621680+sebbyT@users.noreply.github.com>
2021-06-06 22:31:52 +1000

    Update unCaptcha_v9.js

--- unCaptcha_v9.js
+++ unCaptcha_v9.js
@@ -69,8 +69,8 @@ scale(-1,1); // seb's code
 scale(-1,1);
     noFill();
    stroke(230,0,0);
-    rect(leftEye.x+80,leftEye.y+60,40,40);
-    rect(rightEye.x+100,rightEye.y+60,40,40);
+    rect(leftEye.x,leftEye.y,40,40);
+    rect(rightEye.x,rightEye.y,40,40);
     noFill(0);
     pop();
     
